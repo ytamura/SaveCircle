b25724e1f49aada0ef9e002ed2a3cb7a6ce61276
Yuriko <ytamura@gmail.com>
2014-02-08 12:43:08 -0800

    first commit

---
+++ SaveCircle/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  SaveCircle
+//
+//  Created by Yuriko Tamura on 2014/02/08.
+//  Copyright (c) 2014年 FinCapDev Hackathon. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SaveCircle/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SaveCircle
+//
+//  Created by Yuriko Tamura on 2014/02/08.
+//  Copyright (c) 2014年 FinCapDev Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
