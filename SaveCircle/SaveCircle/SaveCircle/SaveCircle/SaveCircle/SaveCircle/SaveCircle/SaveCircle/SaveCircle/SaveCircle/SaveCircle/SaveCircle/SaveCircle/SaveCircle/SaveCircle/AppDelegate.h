209151cb77bf4482e0751cb56f2012ea946c5f13
Yuriko <ytamura@gmail.com>
2014-02-08 16:00:41 -0800

    feed view stuff

---
+++ SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SaveCircle
+//
+//  Created by Yuriko Tamura on 2014/02/08.
+//  Copyright (c) 2014年 FinCapDev Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/SaveCircle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SaveCircle
+//
+//  Created by Yuriko Tamura on 2014/02/08.
+//  Copyright (c) 2014年 FinCapDev Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
