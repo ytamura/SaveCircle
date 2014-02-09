f3dd9e4d8d5c4fa01e9b6169233b8743562f93f0
Yuriko <ytamura@gmail.com>
2014-02-08 19:53:20 -0800

    wish list

--- SaveCircle/WishListViewController.m
+++ SaveCircle/WishListViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "WishListViewController.h"
+#import "AppDelegate.h"
+#import "Goal.h"
 
 @interface WishListViewController ()
 
@@ -24,6 +26,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [SharedAppDelegate.goals count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GoalCell" forIndexPath:indexPath];
+    
+    Goal* g = [SharedAppDelegate.goals objectAtIndex:indexPath.row];
+    
+    UILabel* goalLabel = (UILabel*)[cell viewWithTag:300];
+    [goalLabel setText:g.name];
+
+    UILabel* goalAmountLabel = (UILabel*)[cell viewWithTag:301];
+    NSInteger goalAmount = g.goal_amount_cents / 100;
+    [goalAmountLabel setText:[NSString stringWithFormat:@"$%li", (long)goalAmount]];
+    
+    UIProgressView* goalProgressView = (UIProgressView*)[cell viewWithTag:302];
+    float goalPercent = (float)g.amount_cents_so_far / g.goal_amount_cents;
+    [goalProgressView setProgress:(float)goalPercent animated:YES];
+    
+    UILabel* savedAmountLabel = (UILabel*)[cell viewWithTag:303];
+    NSInteger savedAmount = g.amount_cents_so_far / 100;
+    [savedAmountLabel setText:[NSString stringWithFormat:@"$%li saved", (long)savedAmount]];
+    
+    UILabel* savedPercentLabel = (UILabel*)[cell viewWithTag:304];
+    [savedPercentLabel setText:[NSString stringWithFormat:@"%li%%", (long)goalPercent*100]];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
 
 @end
